640fb373280a4dc9ced83a96670fac64ffdab7a6
yukihane <yukihane.feather@gmail.com>
2020-04-05 14:41:19 +0900

    type annotation

--- typescript/official_handbook/tooling_in_5minites/greeter.ts
+++ typescript/official_handbook/tooling_in_5minites/greeter.ts
@@ -1,7 +1,7 @@
-function greeter(person) {
+function greeter(person: string) {
     return "Hello, " + person;
 }
 
-let user = "Jane User";
+let user = [0, 1, 2];
 
 document.body.textContent = greeter(user);
