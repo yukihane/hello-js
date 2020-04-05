3d15d97e6c21373d5fcecc5b57e767f44d3517b6
yukihane <yukihane.feather@gmail.com>
2020-04-05 14:43:11 +0900

    interfaces

--- typescript/official_handbook/tooling_in_5minites/greeter.ts
+++ typescript/official_handbook/tooling_in_5minites/greeter.ts
@@ -1,7 +1,12 @@
-function greeter(person: string) {
-    return "Hello, " + person;
+interface Person {
+    firstName: string;
+    lastName: string;
 }
 
-let user = [0, 1, 2];
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = { firstName: "Jane", lastName: "User" };
 
 document.body.textContent = greeter(user);
