14e92e7e8cd08655c7756cbb946b7bfc10bcbd7c
yukihane <yukihane.feather@gmail.com>
2020-04-06 17:27:11 +0900

    17.診断機能の組込み

--- nnn.ed.nico/webapp-primer-2020/assessment/assessment.js
+++ nnn.ed.nico/webapp-primer-2020/assessment/assessment.js
@@ -1,4 +1,38 @@
 'use strict';
+
+const userNameInput = document.getElementById('user-name');
+const assessmentButton = document.getElementById('assessment');
+const resutlDivided = document.getElementById('result-area');
+const tweetDivided = document.getElementById('tweet-area');
+
+/**
+ * 指定した要素の子供を全て削除する
+ * @param {HTMLElement} element HTMLの要素
+ */
+function removeAllChildren(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+assessmentButton.onclick = () => {
+    const userName = userNameInput.value;
+    if (userName.length === 0) {
+        return;
+    }
+
+    removeAllChildren(resutlDivided);
+
+    const header = document.createElement('h3');
+    header.innerText = '診断結果';
+    resutlDivided.appendChild(header);
+
+    const paragraph = document.createElement('p');
+    const result = assessment(userName);
+    paragraph.innerText = result;
+    resutlDivided.appendChild(paragraph);
+}
+
 const answers = [
     '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
     '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
@@ -40,4 +74,7 @@ console.assert(
     '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
     '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
 );
-console.log('hello');
+console.assert(
+    assessment('太郎') === assessment('太郎'),
+    '入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
+);
