0cc84a27b4c46ad1b1590ee3e9b392eb8004b481
yukihane <yukihane.feather@gmail.com>
2020-04-07 19:03:00 +0900

    23.データベースへの保存機能の実装

---
+++ nnn.ed.nico/webapp-primer-2020/ubuntu64_18/workspace/secret-board-3022/lib/posts-handler.js
@@ -0,0 +1,49 @@
+'use strict';
+const pug = require('pug');
+const util = require('./handler-util');
+const Post = require('./post');
+
+function handle(req, res) {
+  switch (req.method) {
+    case 'GET':
+      res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8'
+      });
+      Post.findAll().then(posts => {
+        res.end(pug.renderFile('./views/posts.pug', { posts: posts }));
+      });
+      break;
+    case 'POST':
+      let body = [];
+      req.on('data', (chunk) => {
+        body.push(chunk);
+      }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        const decoded = decodeURIComponent(body);
+        const content = decoded.split('content=')[1];
+        console.info('投稿されました: ' + content);
+        Post.create({
+          content: content,
+          trackingCookie: null,
+          postedBy: req.user
+        }).then(() => {
+          handleRedirectPosts(req, res);
+        })
+      });
+      break;
+    default:
+      util.handleBadRequest(req, res);
+      break;
+  }
+}
+
+function handleRedirectPosts(req, res) {
+  res.writeHead(303, {
+    'Location': '/posts'
+  });
+  res.end();
+}
+
+module.exports = {
+  handle
+};
