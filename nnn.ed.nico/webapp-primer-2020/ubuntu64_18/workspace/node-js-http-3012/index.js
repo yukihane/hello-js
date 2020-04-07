c83b1eedfac115a4852be13b1e9e89e2dbeab637
yukihane <yukihane.feather@gmail.com>
2020-04-07 16:17:17 +0900

    13.ログ

---
+++ nnn.ed.nico/webapp-primer-2020/ubuntu64_18/workspace/node-js-http-3012/index.js
@@ -0,0 +1,21 @@
+'use strict';
+const http = require('http');
+const server = http.createServer((req, res) => {
+    console.info(
+        '[' + new Date() + '] Requested by ' + req.connection.remoteAddress
+    );
+    res.writeHead(200, {
+        'Content-Type': 'text/plain; charset=utf-8'
+    });
+    res.write(req.headers['user-agent']);
+    res.end();
+}).on('error', e => {
+    console.error('[' + new Date() + '] Server Error', e);
+}).on('clientError', e => {
+    console.error('[' + new Date() + '] Client Error', e);
+});
+
+const port = 8000;
+server.listen(port, () => {
+    console.log('Listening on ' + port);
+});
