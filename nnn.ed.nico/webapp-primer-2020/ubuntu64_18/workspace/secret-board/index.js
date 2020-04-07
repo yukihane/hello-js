67b5b27c96b909362300ecfe7435606814da00c9
yukihane <yukihane.feather@gmail.com>
2020-04-07 17:50:51 +0900

    21.フォームによる投稿機能の実装

---
+++ nnn.ed.nico/webapp-primer-2020/ubuntu64_18/workspace/secret-board/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const http = require('http');
+const router = require('./lib/router');
+
+const server = http
+    .createServer((req, res) => {
+        router.route(req, res);
+    })
+    .on('error', e => {
+        console.error('Server Error', e);
+    })
+    .on('clientError', e => {
+        console.error('Client Error', e);
+    });
+
+const port = 8000;
+server.listen(port, () => {
+    console.info('Listening on ' + port);
+})
---
+++ nnn.ed.nico/webapp-primer-2020/ubuntu64_18/workspace/secret-board/lib/router.js
@@ -0,0 +1,19 @@
+'use strict';
+const postsHandler = require('./posts-handler');
+
+function route(req, res) {
+    switch (req.url) {
+        case '/posts':
+            postsHandler.handle(req, res);
+            break;
+        case '/logout':
+            // TODO ログアウト処理
+            break;
+        default:
+            break;
+    }
+}
+
+module.exports = {
+    route
+};
