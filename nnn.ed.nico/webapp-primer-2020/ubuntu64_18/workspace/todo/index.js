61edd7fae8edfc1e60ea66fb6b0a664249f5792b
yukihane <yukihane.feather@gmail.com>
2020-04-07 12:58:05 +0900

    11.例外処理

--- nnn.ed.nico/webapp-primer-2020/ubuntu64_18/workspace/todo/index.js
+++ nnn.ed.nico/webapp-primer-2020/ubuntu64_18/workspace/todo/index.js
@@ -1,6 +1,21 @@
 'use strict';
+
+const fs = require('fs');
+const fileName = './tasks.json';
+
 // { name: タスクの文字列, state: 完了しているかどうかの真偽値 }
-const tasks = new Array();
+let tasks = new Array();
+
+try {
+    const data = fs.readFileSync(fileName, 'utf8');
+    tasks = JSON.parse(data);
+} catch (ignore) {
+    console.log(fileName + 'から復元できませんでした');
+}
+
+function saveTasks() {
+    fs.writeFileSync(fileName, JSON.stringify(tasks), 'utf8');
+}
 
 /**
  * TODOを追加する
@@ -8,6 +23,7 @@ const tasks = new Array();
  */
 function todo(task) {
     tasks.push({ name: task, state: false });
+    saveTasks();
 }
 
 /**
@@ -37,6 +53,7 @@ function done(task) {
     const indexFound = tasks.findIndex(t => t.name === task);
     if (indexFound != -1) {
         tasks[indexFound].state = true;
+        saveTasks();
     }
 }
 
@@ -52,6 +69,7 @@ function del(task) {
     const indexFound = tasks.findIndex(t => t.name === task);
     if (indexFound != -1) {
         tasks.splice(indexFound, 1);
+        saveTasks();
     }
 }
 
