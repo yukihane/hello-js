ac970fb1476c02985d177472726669883a527898
yukihane <yukihane.feather@gmail.com>
2020-04-07 15:40:15 +0900

    12.HTTP サーバー

---
+++ nnn.ed.nico/webapp-primer-2020/ubuntu64_18/workspace/node-js-http/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8'
+    });
+    res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>');
+    res.end();
+});
+
+const port = 8000;
+server.listen(port, () => {
+    console.log('Listening on ' + port);
+});
