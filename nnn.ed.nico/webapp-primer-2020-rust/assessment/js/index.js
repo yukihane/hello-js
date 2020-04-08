e4c226c08979707f78adb6dab6bc8228a6b2ea61
yukihane <yukihane.feather@gmail.com>
2020-04-08 18:11:35 +0900

    assessment.jsをコピペ

--- nnn.ed.nico/webapp-primer-2020-rust/assessment/js/index.js
+++ nnn.ed.nico/webapp-primer-2020-rust/assessment/js/index.js
@@ -1,1 +1,53 @@
-import("../pkg/index.js").catch(console.error);
+import("../pkg/index.js").then(module => {
+    const userNameInput = document.getElementById('user-name');
+    const assessmentButton = document.getElementById('assessment');
+    const resultDivided = document.getElementById('result-area');
+    const tweetDivided = document.getElementById('tweet-area');
+
+    /**
+    * 指定した要素の子どもを全て除去する
+    * @param {HTMLElement} element HTMLの要素
+    */
+    function removeAllChildren(element) {
+        while (element.firstChild) { // 子どもの要素があるかぎり削除
+            element.removeChild(element.firstChild);
+        }
+    }
+
+    assessmentButton.onclick = () => {
+        const userName = userNameInput.value;
+        if (userName.length === 0) { // 名前が空の時は処理を終了する
+            return;
+        }
+
+        // 診断結果表示エリアの作成
+        removeAllChildren(resultDivided);
+        const header = document.createElement('h3');
+        header.innerText = '診断結果';
+        resultDivided.appendChild(header);
+
+        const paragraph = document.createElement('p');
+        const result = module.assessment(userName);
+        paragraph.innerText = result;
+        resultDivided.appendChild(paragraph);
+
+        // ツイートエリアの作成
+        removeAllChildren(tweetDivided);
+        const anchor = document.createElement('a');
+        const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag=%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D&text='
+            + encodeURIComponent(result);
+        anchor.setAttribute('href', hrefValue);
+        anchor.className = 'twitter-hashtag-button';
+        anchor.innerText = 'Tweet #%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D';
+        tweetDivided.appendChild(anchor);
+
+        twttr.widgets.load();
+    };
+
+    userNameInput.onkeydown = (event) => {
+        if (event.keyCode === 13) {
+            assessmentButton.onclick();
+        }
+    };
+
+}).catch(console.error);
