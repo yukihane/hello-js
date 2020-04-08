54162d8eb81df7fb06e5838e8fc6695419f7b1f9
yukihane <yukihane.feather@gmail.com>
2020-04-08 18:11:07 +0900

    Rustでassessment関数実装

--- nnn.ed.nico/webapp-primer-2020-rust/assessment/src/lib.rs
+++ nnn.ed.nico/webapp-primer-2020-rust/assessment/src/lib.rs
@@ -1,7 +1,6 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
-
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
 //
@@ -10,18 +9,43 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
-    #[cfg(debug_assertions)]
-    console_error_panic_hook::set_once();
+  // This provides better error messages in debug mode.
+  // It's disabled in release mode so it doesn't bloat up the file size.
+  #[cfg(debug_assertions)]
+  console_error_panic_hook::set_once();
 
+  // Your code goes here!
+  console::log_1(&JsValue::from_str("Hello world!"));
 
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+  Ok(())
+}
 
-    Ok(())
+const ANSWERS :[&str;16] = [
+  "{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。",
+  "{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。",
+  "{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。",
+  "{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。",
+  "{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています。",
+  "{userName}のいいところはユニークさです。{userName}だけのその特徴が皆を楽しくさせます。",
+  "{userName}のいいところは用心深さです。{userName}の洞察に、多くの人が助けられます。",
+  "{userName}のいいところは見た目です。内側から溢れ出る{userName}の良さに皆が気を惹かれます。",
+  "{userName}のいいところは決断力です。{userName}がする決断にいつも助けられる人がいます。",
+  "{userName}のいいところは思いやりです。{userName}に気をかけてもらった多くの人が感謝しています。",
+  "{userName}のいいところは感受性です。{userName}が感じたことに皆が共感し、わかりあうことができます。",
+  "{userName}のいいところは節度です。強引すぎない{userName}の考えに皆が感謝しています。",
+  "{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。",
+  "{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。",
+  "{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。",
+  "{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。"
+];
+
+#[wasm_bindgen]
+pub fn assessment(user_name: &str) -> String {
+  let sum = user_name.chars().map(|x| x as u32).sum();
+  let index = (sum % (ANSWERS.len() as u32)) as usize;
+
+  ANSWERS[index].replace("{userName}", user_name)
 }
