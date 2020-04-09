f0f4452d7534fc29b5aff030509b48d0de21eb6e
yukihane <yukihane.feather@gmail.com>
2020-04-10 02:19:12 +0900

    手番の処理

--- reactjs.org/tutorial/src/index.js
+++ reactjs.org/tutorial/src/index.js
@@ -18,13 +18,17 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({ squares });
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -37,7 +41,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
