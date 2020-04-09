7183c559f374bcbc3efae62c2a673da79fb3977d
yukihane <yukihane.feather@gmail.com>
2020-04-10 02:12:39 +0900

    Stateのリフトアップ

--- reactjs.org/tutorial/src/index.js
+++ reactjs.org/tutorial/src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+
     render() {
         return (
-            <button className="square">
+            <button
+                className="square"
+                onClick={() => this.props.onClick()}
+            >
                 {this.props.value}
             </button>
         );
@@ -13,8 +17,26 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({ squares });
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
