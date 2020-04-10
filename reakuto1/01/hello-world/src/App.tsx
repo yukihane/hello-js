8229a46279762462b91c9f9b3d2c1d969ac66ba8
yukihane <yukihane.feather@gmail.com>
2020-04-10 13:46:18 +0900

    サンプルコードで関数を書き換え

--- reakuto1/01/hello-world/src/App.tsx
+++ reakuto1/01/hello-world/src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const logoAttrs = {
+    alt: "logo",
+    className: "App-logo",
+    src: logo,
+  };
+  const title = " こ ん に ち は React";
+  const targets = ["World", "Kanae", "Yukina"];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          // コ メ ン ト は こ う 書 く
+        }
+        <img {...logoAttrs} />
+        {title && <p>{title}</p>}
+        {targets.map((target) => (
+          <p>Hello, {target}!</p>
+        ))}
       </header>
     </div>
   );
-}
+};
 
 export default App;
