02035a893f336818cfa6ab6c1d1de283bce291ca
yukihane <yukihane.feather@gmail.com>
2020-04-12 08:08:08 +0900

    7-4. コンポーネントのライフサイクル

--- reakuto1/07-04/countdown-timer/src/App.tsx
+++ reakuto1/07-04/countdown-timer/src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Button, Card, Icon, Statistic } from "semantic-ui-react";
+import "./App.css";
+
+const LIMIT = 60;
 
 function App() {
+  const [timeLeft, setTimeLeft] = useState(LIMIT);
+
+  // https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+  useEffect(() => {
+    const tick = () => {
+      setTimeLeft(timeLeft - 1);
+    };
+
+    const timerId = setInterval(tick, 1000);
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [timeLeft]);
+
+  const reset = () => {
+    setTimeLeft(LIMIT);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="number-board">
+      <header>
+        <h1>タイマー</h1>
       </header>
+      <Card>
+        <Statistic className="number-board">
+          <Statistic.Label>time</Statistic.Label>
+          <Statistic.Value>{timeLeft}</Statistic.Value>
+        </Statistic>
+        <Card.Content>
+          <Button color="red" onClick={reset} fluid>
+            <Icon name="redo" />
+          </Button>
+        </Card.Content>
+      </Card>
     </div>
   );
 }
