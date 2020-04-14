8138975e958338cc16166e6417a44c68cee49b7e
yukihane <yukihane.feather@gmail.com>
2020-04-15 03:26:05 +0900

    ダミーコンポーネント作成

---
+++ redux.js.org/basic-tutorial-typescript/src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Todo from "./Todo";
+import TodoList from "./TodoList";
+import Footer from "./Footer";
+
+const App = () => (
+  <div>
+    <Todo />
+    <TodoList />
+    <Footer />
+  </div>
+);
+
+export default App;
---
+++ redux.js.org/basic-tutorial-typescript/src/components/TodoList.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const TodoList = () => <div></div>;
+
+export default TodoList;
