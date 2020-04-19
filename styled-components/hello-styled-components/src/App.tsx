fd035ccc30167b3fa81ec98cb144ecdcadeb626e
yukihane <yukihane.feather@gmail.com>
2020-04-20 03:21:14 +0900

    実装

--- styled-components/hello-styled-components/src/App.tsx
+++ styled-components/hello-styled-components/src/App.tsx
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import logo from "./logo.svg";
+// import "./App.css";
+
+const AppStyle = styled.div`
+  text-align: center;
+`;
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  & code {
+    color: #aaeeff;
+  }
+`;
+const AppLink = styled.a`
+  color: #61dafb;
+  &:hover {
+    color: #00ffff;
+  }
+`;
+const AppLogoSpin = keyframes`
+from {
+  transform: rotate(0deg);
+}
+to {
+  transform: rotate(360deg);
+}
+`;
+
+const AppLogo = styled.img`
+  animation: ${AppLogoSpin} infinite 20s linear;
+  height: 40vmin;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <AppStyle>
+      <AppHeader>
+        <AppLogo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <AppLink
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </AppLink>
+      </AppHeader>
+    </AppStyle>
   );
 }
 
