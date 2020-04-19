e3c6ba3782315a22e91bfe1cb25fae9fbc0a28a6
yukihane <yukihane.feather@gmail.com>
2020-04-19 17:53:57 +0900

    コンパイルエラー解消

--- electron-workshop/day4/src/reducers.ts
+++ electron-workshop/day4/src/reducers.ts
@@ -1,19 +1,17 @@
 import { combineReducers, Reducer } from "redux";
-import { TaskState, createTask } from "./types";
-import {
-  ActionType,
-  TaskAction,
-  addTask,
-  showTasks,
-  toggleCompleteTask,
-  deleteTask,
-} from "./actions";
+import { TaskState, createTask, initTaskState } from "./types";
+import { ActionType, TaskAction } from "./actions";
 
 const task: Reducer<TaskState, TaskAction> = (
-  state: TaskState,
+  state: TaskState | undefined,
   action: TaskAction
 ): TaskState => {
-  const newState = { ...state };
+  let newState: TaskState;
+  if (state) {
+    newState = { ...state };
+  } else {
+    newState = { ...initTaskState };
+  }
 
   switch (action.type) {
     case ActionType.ADD_TASK:
@@ -22,23 +20,23 @@ const task: Reducer<TaskState, TaskAction> = (
       return newState;
     case ActionType.SHOW_TASKS:
       newState.tasks = { ...action.tasks };
-      break;
+      return newState;
     case ActionType.TOGGLE_COMPLETE_TASK:
-      const targetTask = state.tasks.find((t) => t.id === action.taskId);
+      const targetTask = newState.tasks.find((t) => t.id === action.taskId);
       if (!targetTask) {
-        return state;
+        return state || initTaskState;
       }
       targetTask.complete = !targetTask.complete;
       return newState;
     case ActionType.DELETE_TASK:
-      const deleteTarget = state.tasks.find((t) => t.id === action.taskId);
+      const deleteTarget = newState.tasks.find((t) => t.id === action.taskId);
       if (!deleteTarget) {
-        return state;
+        return state || initTaskState;
       }
-      state.tasks = state.tasks.filter((it) => it.id !== action.taskId);
+      newState.tasks = newState.tasks.filter((it) => it.id !== action.taskId);
       return newState;
     default:
-      return state;
+      return initTaskState;
   }
 };
 
