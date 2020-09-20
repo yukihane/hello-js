74b1705cbf680241644f5ad92eb1a4059bd3696b
yukihane <yukihane.feather@gmail.com>
2020-09-20 22:30:19 +0900

    component分離

--- react-basics/hello-router/src/App.tsx
+++ react-basics/hello-router/src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Home } from "./Home";
+
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
