db264ea8f06c2e7df81d123c365f46e2383e9445
yukihane <yukihane.feather@gmail.com>
2020-09-20 09:26:46 +0900

    viewportデフォルト設定 https://storybook.js.org/docs/react/essentials/viewport

--- hello-design/.storybook/preview.js
+++ hello-design/.storybook/preview.js
@@ -1,4 +1,9 @@
+import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  // https://storybook.js.org/docs/react/essentials/viewport
+  viewport: {
+    viewports: MINIMAL_VIEWPORTS,
+  },
+};
