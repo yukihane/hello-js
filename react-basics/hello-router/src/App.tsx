ed0a4f3ded3ad44fd384a316e89216fc2c4671ed
yukihane <yukihane.feather@gmail.com>
2020-09-20 22:34:51 +0900

    first example https://reactrouter.com/web/guides/quick-start

--- react-basics/hello-router/src/App.tsx
+++ react-basics/hello-router/src/App.tsx
@@ -2,10 +2,47 @@ import React from "react";
 import "./App.css";
 import { Home } from "./Home";
 
-// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
