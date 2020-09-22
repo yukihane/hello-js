93c075b2972d0facd52237c02f40c0508bc9f51e
yukihane <yukihane.feather@gmail.com>
2020-09-22 13:24:22 +0900

    バリデーションを適用する

--- react-hook-form-example/src/App.tsx
+++ react-hook-form-example/src/App.tsx
@@ -1,35 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { useForm } from "react-hook-form";
 
-enum GenderEnum {
-  female = "female",
-  male = "male",
-  other = "other",
-}
-
 interface IFormInput {
-  firstName: String;
-  gender: GenderEnum;
+  firstName: string;
+  lastName: string;
+  age: number;
 }
 
 export default function App() {
   const { register, handleSubmit } = useForm<IFormInput>();
-
-  const onSubmit = (data: IFormInput) => {
-    console.log(data);
-  };
+  const onSubmit = (data: IFormInput) => console.log(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <label>First Name</label>
-      <input name="firstName" ref={register} />
-      <label>Gender Selection</label>
-      <select name="gender" ref={register}>
-        <option value="female">female</option>
-        <option value="male">male</option>
-        <option value="other">other</option>
-      </select>
+      <input
+        name="firstName"
+        ref={register({ required: true, maxLength: 20 })}
+      />
+      <input name="lastName" ref={register({ pattern: /^[A-Za-z]+$/i })} />
+      <input name="age" type="number" ref={register({ min: 18, max: 99 })} />
       <input type="submit" />
     </form>
   );
