2ca09e0fdc92c2548ea03c07a410b78ad207506f
yukihane <yukihane.feather@gmail.com>
2021-08-07 08:45:16 +0900

    Instructions.js

---
+++ oreilly-react-hands-on-learning-2nd/05.3/src/components/Instructions.js
@@ -0,0 +1,10 @@
+export default function Instructions({ title, steps }) {
+  return (
+    <section className="instructions">
+      <h2>{title}</h2>
+      {steps.map((s, i) => (
+        <p key={i}>{s}</p>
+      ))}
+    </section>
+  );
+}
