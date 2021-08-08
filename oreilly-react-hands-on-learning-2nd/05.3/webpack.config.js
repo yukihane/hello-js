9fed473f75bb8e469b66741ba3e06d55a982b1fc
yukihane <yukihane.feather@gmail.com>
2021-08-09 04:32:57 +0900

    babel-loader初期設定

--- oreilly-react-hands-on-learning-2nd/05.3/webpack.config.js
+++ oreilly-react-hands-on-learning-2nd/05.3/webpack.config.js
@@ -6,4 +6,7 @@ module.exports = {
     path: path.join(__dirname, "dist", "assets"),
     filename: "bundle.js",
   },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }],
+  },
 };
