32659cb113346b1edf0b21baf8031ae790efd2d0
yukihane <yukihane.feather@gmail.com>
2021-08-09 05:11:24 +0900

    starを可変に

--- oreilly-react-hands-on-learning-2nd/06/src/Star.tsx
+++
@@ -1,5 +0,0 @@
-import { FaStar } from "react-icons/fa";
-
-const Star = ({ selected = false }) => (
-  <FaStar color={selected ? "red" : "grey"} />
-);
--- oreilly-react-hands-on-learning-2nd/06/src/StarRating.tsx
+++ oreilly-react-hands-on-learning-2nd/06/src/StarRating.tsx
@@ -1,10 +1,8 @@
 import { FaStar } from "react-icons/fa";
-export const StarRating = () => {
-  return [
-    <FaStar color="red" />,
-    <FaStar color="red" />,
-    <FaStar color="red" />,
-    <FaStar color="grey" />,
-    <FaStar color="grey" />,
-  ];
+export const StarRating = ({ totalStarts = 5 }) => {
+  return [...Array(totalStarts)].map((_, i) => <Star key={i} />);
 };
+
+const Star = ({ selected = false }) => (
+  <FaStar color={selected ? "red" : "grey"} />
+);
