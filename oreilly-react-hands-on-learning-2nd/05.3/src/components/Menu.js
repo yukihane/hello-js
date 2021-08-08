9a4bbd687995665cfb589e2bd5071c38ab42fe18
yukihane <yukihane.feather@gmail.com>
2021-08-09 04:24:02 +0900

    Menu

---
+++ oreilly-react-hands-on-learning-2nd/05.3/src/components/Menu.js
@@ -0,0 +1,16 @@
+function Menu({ recipes }) {
+  return (
+    <article>
+      <header>
+        <h1>Delicious Recipes</h1>
+      </header>
+      <div className="recipes">
+        {recipes.map((recipe, i) => (
+          <Recipe key={i} {...recipe} />
+        ))}
+      </div>
+    </article>
+  );
+}
+
+export default Menu;
