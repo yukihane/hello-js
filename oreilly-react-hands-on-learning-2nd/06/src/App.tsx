30cfe2c4b20103f704c70b7b4559fd07c07f74ed
yukihane <yukihane.feather@gmail.com>
2021-08-11 00:26:34 +0900

    入力をステート値に反映させる

--- oreilly-react-hands-on-learning-2nd/06/src/App.tsx
+++ oreilly-react-hands-on-learning-2nd/06/src/App.tsx
@@ -2,12 +2,33 @@ import React from "react";
 import colorData from "./color-data.json";
 import { ColorList } from "./ColorList";
 import { useState } from "react";
+import { AddColorForm } from "./AddColorForm";
+import { v4 } from "uuid";
 
 export type ColorDataType = typeof colorData[0];
 
 const App = () => {
-  const [colors] = useState(colorData);
-  return <ColorList colors={colors} />;
+  const [colors, setColors] = useState(colorData);
+
+  return (
+    <>
+      <AddColorForm
+        onNewColor={(title, color) => {
+          const newColors = [
+            ...colors,
+            {
+              id: v4(),
+              rating: 0,
+              title,
+              color,
+            },
+          ];
+          setColors(newColors);
+        }}
+      />
+      <ColorList colors={colors} />
+    </>
+  );
 };
 
 export default App;
