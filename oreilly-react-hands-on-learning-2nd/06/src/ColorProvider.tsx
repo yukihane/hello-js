d68ff11904e6527f640ccdcf9b730a3189884cd9
yukihane <yukihane.feather@gmail.com>
2021-08-11 09:46:04 +0900

    6.4.3

---
+++ oreilly-react-hands-on-learning-2nd/06/src/ColorProvider.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { FC } from "react";
+import { createContext, useState } from "react";
+import { v4 } from "uuid";
+import colorData from "./color-data.json";
+
+export type ColorDataType = typeof colorData[0];
+
+export type ColorContextType = {
+  colors: ColorDataType[];
+  addColor: (title: string, color: string) => void;
+  removeColor: (id: string) => void;
+  rateColor: (id: string, rating: number) => void;
+};
+
+export const ColorContext = createContext<ColorContextType>({
+  colors: [],
+  addColor: (title, color) => {},
+  removeColor: (id) => {},
+  rateColor: (id, rating) => {},
+});
+
+export const ColorProvider: FC = ({ children }) => {
+  const [colors, setColors] = useState(colorData);
+
+  const addColor = (title: string, color: string) =>
+    setColors([
+      ...colors,
+      {
+        id: v4(),
+        rating: 0,
+        title,
+        color,
+      },
+    ]);
+
+  const rateColor = (id: string, rating: number) =>
+    setColors(
+      colors.map((color) => (color.id === id ? { ...color, rating } : color))
+    );
+
+  const removeColor = (id: string) =>
+    setColors(colors.filter((color) => color.id !== id));
+
+  return (
+    <ColorContext.Provider value={{ colors, addColor, removeColor, rateColor }}>
+      {children}
+    </ColorContext.Provider>
+  );
+};
--- oreilly-react-hands-on-learning-2nd/06/src/index.tsx
+++ oreilly-react-hands-on-learning-2nd/06/src/index.tsx
@@ -3,16 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createContext } from "react";
-import colors from "./color-data.json";
-
-export type ColorDataType = typeof colors[0];
-
-export type ColorContextType = {
-  colors: ColorDataType[];
-};
-
-export const ColorContext = createContext<ColorContextType>({ colors: [] });
 
 ReactDOM.render(
   <React.StrictMode>
