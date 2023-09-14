ab1b8e355c64eac0ce16e07c6c563b7e5642b98d
yukihane <yukihane.feather@gmail.com>
2023-09-15 01:58:38 +0900

    GraphQL サーバーの作成

---
+++ apollo/hello-apollo/src/pages/api/graphql.ts
@@ -0,0 +1,22 @@
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { readFileSync } from "fs";
+import { join } from "path";
+import { Resolvers } from "../../../apollo/__generated__/server/resolvers-types";
+
+const typeDefs = readFileSync(
+  join(process.cwd(), "apollo/documents/schema.gql"),
+  "utf-8"
+);
+
+const resolvers: Resolvers = {
+  Query: {
+    users() {
+      return [{ name: "Nextjs" }, { name: "Nuxtjs" }, { name: "Sveltekit" }];
+    },
+  },
+};
+
+const apolloServer = new ApolloServer<Resolvers>({ typeDefs, resolvers });
+
+export default startServerAndCreateNextHandler(apolloServer);
