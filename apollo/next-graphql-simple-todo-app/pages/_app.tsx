5621ebdfdd04183c9740cc980e1bcfd4051e2aae
yukihane <yukihane.feather@gmail.com>
2024-01-04 03:56:17 +0900

    ChakraProviderでAppコンポーネントを囲んでおきます

--- apollo/next-graphql-simple-todo-app/pages/_app.tsx
+++ apollo/next-graphql-simple-todo-app/pages/_app.tsx
@@ -1,13 +1,15 @@
 import "@/styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-
 import apolloClient from "../lib/apollo";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </ApolloProvider>
   );
 }
