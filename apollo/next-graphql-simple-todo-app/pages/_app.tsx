10689daae3f89e34026c47f4b2e59ce25094314a
yukihane <yukihane.feather@gmail.com>
2024-01-04 03:53:09 +0900

    各コンポーネントからApolloClientを使用してGraphQLクエリを送ることができるようにします

--- apollo/next-graphql-simple-todo-app/pages/_app.tsx
+++ apollo/next-graphql-simple-todo-app/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
+import type { AppProps } from "next/app";
+
+import apolloClient from "../lib/apollo";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
