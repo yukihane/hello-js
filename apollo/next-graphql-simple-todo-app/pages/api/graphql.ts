f7713fa417b8e3d8568b91218d1d14e09ae4c4ef
yukihane <yukihane.feather@gmail.com>
2024-01-03 21:02:12 +0900

    ApolloServerにcontextを指定します

--- apollo/next-graphql-simple-todo-app/pages/api/graphql.ts
+++ apollo/next-graphql-simple-todo-app/pages/api/graphql.ts
@@ -1,11 +1,16 @@
 import { ApolloServer } from "apollo-server-micro";
-import { typeDefs } from "../../graphql/schema";
-import { resolvers } from "../../graphql/resolvers";
 import Cors from "micro-cors";
+import { createContext } from "../../graphql/context";
+import { resolvers } from "../../graphql/resolvers";
+import { typeDefs } from "../../graphql/schema";
 
 const cors = Cors();
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: createContext,
+});
 const startServer = apolloServer.start();
 
 export default cors(async function handler(req, res) {
