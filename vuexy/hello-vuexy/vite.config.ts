e5e6689a30a51143d76ff8d098efa7e258f28540
yukihane <yukihane.feather@gmail.com>
2024-10-27 19:46:04 +0900

    index.htmlで実装することにする

--- vuexy/hello-vuexy/vite.config.ts
+++ vuexy/hello-vuexy/vite.config.ts
@@ -1,13 +1,7 @@
 import type { UserConfig } from "vite";
 
 export default {
-  // ...
   build: {
     minify: false,
-    rollupOptions: {
-      input: {
-        "path1/index": "html/path1/index.html",
-      },
-    },
   },
 } satisfies UserConfig;
