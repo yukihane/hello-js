9b47354b2daef67295b5dc34e3e74503c43ba2ab
yukihane <yukihane.feather@gmail.com>
2024-10-27 20:12:22 +0900

    出力ファイル名にhashを用いない

--- vuexy/hello-vuexy/vite.config.ts
+++ vuexy/hello-vuexy/vite.config.ts
@@ -3,5 +3,12 @@ import type { UserConfig } from "vite";
 export default {
   build: {
     minify: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    }
   },
 } satisfies UserConfig;
