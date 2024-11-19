d9a79be5e1d789a23fa906c4b3db009e2ac6df26
yukihane <yukihane.feather@gmail.com>
2024-11-19 10:17:02 +0900

    新規登録行を表示

--- table-row-editable/src/main.ts
+++ table-row-editable/src/main.ts
@@ -2,9 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById("button") as HTMLButtonElement;
   const template = document.getElementById("template") as HTMLTemplateElement;
   const table = document.getElementById("tbody") as HTMLTableElement;
+  var count = 0;
 
   button.addEventListener("click", () => {
-    const clone = template.content.cloneNode(true);
+    const clone = template.content.cloneNode(true) as DocumentFragment;
+    const tr = clone.querySelector("tr") as HTMLTableRowElement;
+    tr.classList.add(`row-${count++}`);
     table.insertBefore(clone, table.firstChild);
   });
 });
